fe79ab3604497fba07196f44295c143687739f3c
Gautami56 <gautu.pinkyar@gmail.com>
2023-09-16 14:17:50 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saving new next 123 to reload.
+          Edit <code>src/App.js</code> and saving new next abcde to reload.
         </p>
         <a
           className="App-link"
